ec880747cb2ce4665728bfb26f83cf4c64e1ccb9
liubavabarsegian <lyubahemmo@mail.ru>
2022-11-06 23:02:50 +0300

    lab is fiished, without rubocop, without tests, with validations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "articles#index"
   #get "/forms", to: "forms#form"
 
+  get 'index' => 'articles#index'
   get 'result' => 'results#result'
   get 'form' => 'forms#form'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
